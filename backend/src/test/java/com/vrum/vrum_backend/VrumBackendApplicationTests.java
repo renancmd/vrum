49af08b46fe95743e21bb8df9d4997703dbd7ff7
Renan <renanmendes421@icloud.com>
2025-06-25 10:52:35 -0300

    build: configure initial spring boot project and postgres via docker-compose

---
+++ backend/src/test/java/com/vrum/vrum_backend/VrumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrum.vrum_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
